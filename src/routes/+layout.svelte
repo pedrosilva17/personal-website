1cae2384ded3314fb634baef51c2f82de24b5a54
pedrosilva17 <pedroafsuper@gmail.com>
2023-12-20 22:59:30 +0000

    build: added tailwind and its dependencies

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,21 @@
+<script lang="ts">
+    import "../app.css";
+</script>
+
+<header>Header</header>
+<main class="flex-grow">
+    <slot />
+</main>
+<footer>Footer</footer>
+
+<style lang="postcss">
+    :global(html) {
+      min-height: 100vh;
+    }
+  
+    :global(body) {
+      min-height: 100vh;
+      display: flex;
+      flex-direction: column;
+    }
+</style>
