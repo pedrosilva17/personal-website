baee2349d69ec9233a4b7e283c54a7f62713d731
pedrosilva17 <pedroafsuper@gmail.com>
2024-01-21 22:53:41 +0000

    fix: theme and font configuration

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,24 +2,24 @@
 export default {
 	content: ['./src/**/*.{html,js,svelte,ts}'],
 	theme: {
-    colors: {},
+		colors: {},
 		extend: {
-      fontFamily: {
-        primary: ['IBM Plex Mono', 'sans-serif'],
-      },
-    }
+			fontFamily: {
+				primary: ['IBM Plex Mono', 'sans-serif']
+			}
+		}
 	},
 	plugins: [require('daisyui')],
-  daisyui: {
-    themes:[
-      {
-        custom: {
-          primary: "#22092C",
-          "primary-content": "#F8F4E3",
-          secondary: "#960200",
-        },
-      },
-      "custom"
-    ]
-  },
+	daisyui: {
+		themes: [
+			{
+				custom: {
+					primary: '#22092C',
+					'primary-content': '#F8F4E3',
+					secondary: '#960200'
+				}
+			},
+			'custom'
+		]
+	}
 };
