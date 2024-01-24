585d4034462e70981723ffaea128ab165ca4ac7d
pedrosilva17 <pedroafsuper@gmail.com>
2024-01-24 20:06:45 +0000

    fix: page layout vertically centered again

--- src/lib/layout/Page.svelte
+++ src/lib/layout/Page.svelte
@@ -6,7 +6,7 @@
 	<title>user@{title.toLowerCase()}</title>
 </svelte:head>
 <section
-	class="flex flex-grow flex-col max-w-[95%] md:max-w-[90%] mx-auto my-5 pt-5 text-lg text-white text-center gap-5"
+	class="flex flex-grow flex-col max-w-[95%] md:max-w-[90%] m-auto pt-5 text-lg text-white justify-center text-center gap-5"
 >
 	<slot />
 </section>
