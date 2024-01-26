1bd6644aa33fb013643bc277c75a97b40c9d896f
pedrosilva17 <pedroafsuper@gmail.com>
2024-01-26 00:42:17 +0000

    feat: add component to setup simple threlte scene

---
+++ src/lib/3d/ModelWrapper.svelte
@@ -0,0 +1,31 @@
+<script lang="ts">
+	import { T, useTask } from '@threlte/core';
+    import { OrbitControls, interactivity } from '@threlte/extras';
+	import { AmbientLight } from 'three';
+
+    interactivity();
+	let rotation = 0
+  	useTask((delta) => {
+    	rotation += delta
+  	})
+</script>
+
+<T.DirectionalLight position={[0, 10, 10]} />
+<T.AmbientLight />
+
+<T.PerspectiveCamera
+  makeDefault
+  position={[0, 2, 15]}
+  on:create={({ ref }) => {
+    ref.lookAt(0, 0, 0)
+  }}
+>
+  <OrbitControls enableDamping />
+</T.PerspectiveCamera>
+
+<T.Mesh
+  	rotation.y={rotation/2}
+>
+	<slot />
+	<T.MeshStandardMaterial />
+</T.Mesh>
