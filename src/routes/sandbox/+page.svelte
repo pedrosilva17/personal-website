0c8e6f94d7eef8c055fe3abe5457bc5c3cf78577
pedrosilva17 <pedroafsuper@gmail.com>
2024-01-30 16:57:26 +0000

    feat: sandbox simple index page

--- src/routes/sandbox/+page.svelte
+++ src/routes/sandbox/+page.svelte
@@ -5,4 +5,15 @@
 <Page title="Sandbox">
 	<h1 class="text-6xl md:text-8xl">Sandbox</h1>
 	<p>Random tools and playthings.</p>
+	<div class="flex flex-wrap justify-center">
+		<a
+			href="/sandbox/number-facts"
+			class="flex card shine w-full md:w-96 bg-primary-variant text-center items-center"
+		>
+			<section class="card-body items-center">
+				<h2 class="card-title">Number Facts</h2>
+				<p>A compilation of facts and curiosities about integers from 1 to 9999.</p>
+			</section>
+		</a>
+	</div>
 </Page>
