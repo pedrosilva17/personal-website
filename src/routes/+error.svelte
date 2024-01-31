aa6fb67a7f5b91b046566a079248ba6337cd3cd0
pedrosilva17 <pedroafsuper@gmail.com>
2024-01-31 15:48:00 +0000

    feat: simple error page

---
+++ src/routes/+error.svelte
@@ -0,0 +1,8 @@
+<script lang="ts">
+    import Page from '$lib/layout/Page.svelte';
+</script>
+
+<Page title="404">
+    <h1 class="text-6xl md:text-8xl">Where are you going?</h1>
+    <p>Hmmm, there's not supposed to be a page here. You should go back!</p>
+</Page>
