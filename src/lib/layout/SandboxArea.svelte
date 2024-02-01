ec73aa7e20e924a056c357cd9ac13937ced28203
pedrosilva17 <pedroafsuper@gmail.com>
2024-02-01 23:49:23 +0000

    feat: add acessibility labels for buttons, change 'help' to 'information'

--- src/lib/layout/SandboxArea.svelte
+++ src/lib/layout/SandboxArea.svelte
@@ -1,40 +1,43 @@
 <script lang="ts">
 	import Page from './Page.svelte';
 	import ArrowBack from 'svelte-ionicons/ArrowBack.svelte';
-	import Help from 'svelte-ionicons/Help.svelte';
+	import Information from 'svelte-ionicons/Information.svelte';
 	export let title: string;
 
 	function toggleModal() {
-		let modal = document.getElementById("help_modal");
+		let modal = document.getElementById('help_modal');
 		if (!modal) return;
-		if (modal.style.visibility !== "visible") {
+		if (modal.style.visibility !== 'visible') {
 			modal.showModal();
-			modal.style.visibility = "visible";
-		}
-		else {
+			modal.style.visibility = 'visible';
+		} else {
 			modal.close();
-			modal.style.visibility = "hidden";
+			modal.style.visibility = 'hidden';
 		}
 	}
 </script>
 
 <Page title="sandbox/{title}">
-	<a href="/sandbox" class="absolute top-0 left-0 btn btn-circle btn-secondary text-primary m-5"
+	<a
+		href="/sandbox"
+		aria-label="Return"
+		class="absolute top-0 left-0 btn btn-circle btn-secondary text-primary m-5"
 		><ArrowBack tabindex="-1" /></a
 	>
 	<button
 		on:click={toggleModal}
+		aria-label="Information"
 		class="absolute top-0 right-0 btn btn-circle btn-secondary text-primary m-5"
-		><Help tabindex="-1" /></button
+		><Information tabindex="-1" /></button
 	>
 	<dialog id="help_modal" tabindex="-1" class="invisible modal m-auto gap-5">
 		<div class="modal-box m-auto bg-primary">
-			<slot name="modal"/>
+			<slot name="modal" />
 			<button on:click={toggleModal} class="btn btn-secondary text-primary">Close</button>
 		</div>
 		<form class="modal-backdrop">
 			<button on:click={toggleModal} tabindex="-1">close</button>
 		</form>
 	</dialog>
-	<slot name="content"/>
+	<slot name="content" />
 </Page>
