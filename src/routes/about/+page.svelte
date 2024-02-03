7aca2af199b84789ae8d67bebf89912f5097ed62
pedrosilva17 <pedroafsuper@gmail.com>
2024-02-03 20:43:45 +0000

    fix: canvas area sizes in about page

--- src/routes/about/+page.svelte
+++ src/routes/about/+page.svelte
@@ -50,7 +50,7 @@
 				</p>
 			</section>
 			<div
-				class="relative flex flex-col m-auto w-1/2 lg:w-1/3 h-48 lg:h-full cursor-grab pointer-events-auto"
+				class="relative flex flex-col m-auto w-full md:w-1/2 lg:w-1/3 h-48 lg:h-full cursor-grab pointer-events-auto"
 			>
 				<Canvas>
 					<ModelWrapper>
@@ -107,7 +107,7 @@
 				</p>
 			</section>
 			<div
-				class="relative flex flex-col m-auto w-1/2 lg:w-1/3 h-48 lg:h-full cursor-grab pointer-events-auto"
+				class="relative flex flex-col m-auto w-full md:w-1/2 lg:w-1/3 h-48 lg:h-full cursor-grab pointer-events-auto"
 			>
 				<Canvas>
 					<ModelWrapper>
