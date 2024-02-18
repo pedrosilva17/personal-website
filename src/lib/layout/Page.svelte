ef81024065c739405f4a665b373d44f637c9d69f
pedrosilva17 <pedroafsuper@gmail.com>
2024-02-18 02:32:26 +0000

    feat: updated favicon and changed head content

--- src/lib/layout/Page.svelte
+++ src/lib/layout/Page.svelte
@@ -3,7 +3,7 @@
 </script>
 
 <svelte:head>
-	<title>user@site:~/{title.toLowerCase().split(' ').join('-')}</title>
+	<title>~/{title.toLowerCase().split(' ').join('-')}</title>
 </svelte:head>
 <section
 	class="flex flex-grow flex-col max-w-[95%] md:max-w-[90%] m-auto py-5 text-lg text-white justify-center text-center gap-5 {$$props.class ??
