54a353f8ac57986c3833e16eca75baf6e48b3751
pedrosilva17 <pedroafsuper@gmail.com>
2024-02-21 02:59:34 +0000

    refactor: simpler addition of new cards

--- src/routes/sandbox/+page.svelte
+++ src/routes/sandbox/+page.svelte
@@ -1,20 +1,33 @@
 <script lang="ts">
 	import MainHeading from '$lib/layout/MainHeading.svelte';
 	import Page from '$lib/layout/Page.svelte';
+	let sandbox_pages = [
+		{
+			name: 'Number Facts',
+			description: 'A compilation of facts and curiosities about integers from 1 to 9999.'
+		},
+		{
+			name: 'Threlte Playground',
+			description:
+				'An interactive Threlte (three.js + Svelte) scene, where I explore different aspects of the library.'
+		}
+	];
 </script>
 
 <Page title="Sandbox">
 	<MainHeading>Sandbox</MainHeading>
 	<p>Random tools and playthings.</p>
-	<div class="flex flex-wrap justify-center">
-		<a
-			href="/sandbox/number-facts"
-			class="flex card shine w-full md:w-96 bg-primary-variant text-center items-center"
-		>
-			<section class="card-body items-center">
-				<h2 class="card-title">Number Facts</h2>
-				<p>A compilation of facts and curiosities about integers from 1 to 9999.</p>
-			</section>
-		</a>
+	<div class="flex flex-wrap justify-center gap-5">
+		{#each sandbox_pages as page}
+			<a
+				href="/sandbox/{page.name.toLowerCase().split(' ').join('-')}"
+				class="flex card shine w-full md:w-96 bg-primary-variant text-center items-center"
+			>
+				<section class="card-body items-center">
+					<h2 class="card-title">{page.name}</h2>
+					<p>{page.description}</p>
+				</section>
+			</a>
+		{/each}
 	</div>
 </Page>
