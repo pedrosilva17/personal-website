32d8daa176ee186ccb300501637f17d087cffeb1
pedrosilva17 <pedroafsuper@gmail.com>
2024-02-22 01:22:38 +0000

    refactor: one folder open at a time (radio input)

--- src/lib/3d/GuiFolder.svelte
+++ src/lib/3d/GuiFolder.svelte
@@ -3,9 +3,9 @@
 </script>
 
 <div class="collapse collapse-arrow p-0 rounded-none">
-	<input tabindex="-1" name={title} type="checkbox" />
+	<input tabindex="-1" name="radio" type="radio" />
 	<label for={title} class="collapse-title md:text-xl flex w-full p-auto">{title}</label>
-	<div class="collapse-content flex flex-col gap-5 max-h-48 md:max-h-96 overflow-y-scroll">
+	<div tabindex="-1" class="collapse-content flex flex-col gap-5 max-h-48 md:max-h-96 overflow-y-scroll pt-1">
 		<slot name="content" />
 	</div>
 </div>
