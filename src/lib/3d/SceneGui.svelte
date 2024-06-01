b903a3260187c4429379968eb3fcd5a9faabebcd
Pedro Silva <pedroafsuper@gmail.com>
2024-06-01 18:43:52 +0100

    fix: GUI dropdown focus outline with correct color

--- src/lib/3d/SceneGui.svelte
+++ src/lib/3d/SceneGui.svelte
@@ -15,13 +15,9 @@
 
 <div
 	class="collapse collapse-arrow absolute mx-auto top-0 right-0 max-md:left-0 w-36 md:w-64 bg-secondary text-primary rounded-xl text-left break-words opacity-80 md:opacity-100 z-10"
+	style="outline-color: oklch(var(--s))"
 >
-	<input
-		on:click={toggleFocusable}
-		name={title}
-		type="checkbox"
-		class="focus:ring-1 focus:ring-secondary"
-	/>
+	<input on:click={toggleFocusable} name={title} type="checkbox" />
 	<label for={title} class="collapse-title text-base md:text-2xl text-center font-bold"
 		>{title}</label
 	>
