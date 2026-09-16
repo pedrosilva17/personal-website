ab62506224c82ee11f3d22c1524d91a363ba0ad4
Pedro Silva <pedroafsilva17@outlook.com>
2026-09-16 11:38:36 +0100

    build: add and configure wrangler

--- src/app.d.ts
+++ src/app.d.ts
@@ -2,11 +2,17 @@
 // for information about these interfaces
 declare global {
 	namespace App {
+		interface Platform {
+			env: Env;
+			ctx: ExecutionContext;
+			caches: CacheStorage;
+			cf?: IncomingRequestCfProperties;
+		}
+
 		// interface Error {}
 		// interface Locals {}
 		// interface PageData {}
 		// interface PageState {}
-		// interface Platform {}
 	}
 }
 
